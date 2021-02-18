71baf8ab0249ddf90e1f8f28af0ee520cbbd13df
Przemysław Gawlas <przemyslaw.gawlas@klgsolutions.com>
2021-02-18 16:18:36 +0100

    Storybook

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,7 @@
+import { React } from 'react';
+import { Button as ButtonMUI } from '@material-ui/core';
+
+// eslint-disable-next-line
+const Button = (props) => <ButtonMUI {...props} />;
+
+export default Button;
