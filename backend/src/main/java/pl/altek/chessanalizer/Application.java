2c9e57d6fbbc6cb51cb3b30eed49a267e9591a1e
Przemysław Gawlas <prgawlas@gmail.com>
2021-12-25 17:57:16 +0100

    Chess.com api rest client

--- backend/src/main/java/pl/altek/chessanalizer/Application.java
+++ backend/src/main/java/pl/altek/chessanalizer/Application.java
@@ -2,14 +2,6 @@ package pl.altek.chessanalizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import pl.altek.chessanalizer.entity.MoveRelation;
-import pl.altek.chessanalizer.entity.StateNode;
-import pl.altek.chessanalizer.enumerate.Player;
-import pl.altek.chessanalizer.repository.StateRepository;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -17,34 +9,42 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	public String test(StateRepository stateRepository) {
-		StateNode node = new StateNode();
-		node.setFen("abc123/asd312");
-		node.setHash("sdjkfhiuwerfis");
-		node.setNextPlayer(Player.BLACK);
-		stateRepository.save(node);
+//	@Bean("myTest")
+//	public String myTest(PlayerApi playerApi){
+//		GameList gameList = playerApi.playerGameMonthlyArchive("altek42", 2021, 10);
+//		Game game = gameList.getGames().get(0);
+//		return game.getFen();
+//	}
+//
+//	@Bean
+//	public String startTest(@Qualifier("myTest") String myTest){
+//		return myTest;
+//	}
 
-		MoveRelation moveRelation = new MoveRelation();
-		moveRelation.setState(node);
-		moveRelation.setName("d4");
-		moveRelation.setQuantity(2L);
-		List<MoveRelation> moves = new ArrayList<>() ;
-		moves.add(moveRelation);
+//	@Bean
+//	public String test(StateRepository stateRepository) {
+//		StateNode node = new StateNode();
+//		node.setFen("abc123/asd312");
+//		node.setHash("sdjkfhiuwerfis");
+//		node.setNextPlayer(Player.BLACK);
+//		stateRepository.save(node);
+//
+//		MoveRelation moveRelation = new MoveRelation();
+//		moveRelation.setState(node);
+//		moveRelation.setName("d4");
+//		moveRelation.setQuantity(2L);
+//		List<MoveRelation> moves = new ArrayList<>() ;
+//		moves.add(moveRelation);
+//
+//		StateNode node2 = new StateNode();
+//		node2.setFen("angoidrng/sdfjos");
+//		node2.setHash("goirmboifm");
+//		node2.setNextPlayer(Player.WHITE);
+//		node2.setMoves(moves);
+//		stateRepository.save(node2);
+//
+//		System.out.println("TEST");
+//		return "OK";
+//	}
 
-		StateNode node2 = new StateNode();
-		node2.setFen("angoidrng/sdfjos");
-		node2.setHash("goirmboifm");
-		node2.setNextPlayer(Player.WHITE);
-		node2.setMoves(moves);
-		stateRepository.save(node2);
-
-		System.out.println("TEST");
-		return "OK";
-	}
-
-	@Bean
-	public String tSt(String test){
-		return test;
-	}
 }
---
+++ backend/src/main/java/pl/altek/chessanalizer/delegate/TestEnDelegate.java
@@ -0,0 +1,9 @@
+package pl.altek.chessanalizer.delegate;
+
+import org.springframework.stereotype.Service;
+import pl.altek.chessanalizer.openapi.api.TestEnApiDelegate;
+
+@Service
+public class TestEnDelegate implements TestEnApiDelegate {
+
+}
