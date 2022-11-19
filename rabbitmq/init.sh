8aa7b643f492ec099dfa415493a1fb07d0050015
Przemysław Gawlas <prgawlas@gmail.com>
2022-11-19 13:06:07 +0100

    Rabbitmq docker config

---
+++ rabbitmq/init.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+rabbitmqadmin --bash-completion
+
+USER_NAME=rabbitmq
+USER_PASS=qwerty123
+
+function setup() {
+  declare -A users
+  users["chessanalizer"]=qwerty123
+  for key in ${!users[@]}; do
+    rabbitmqctl add_user ${key} ${users[${key}]}
+    rabbitmqctl set_user_tags ${key} application
+    rabbitmqctl set_permissions -p / ${key} ".*" ".*" ".*"
+  done
+
+  queue_names=( update-chess )
+  for name in "${queue_names[@]}"
+  do
+    rabbitmqadmin -u $USER_NAME -p $USER_PASS declare queue name=$name durable=true
+  done
+}
+
+( rabbitmqctl wait --timeout 60 $RABBITMQ_PID_FILE ; \
+setup ) &
+
+rabbitmq-server $@
